0147a4e45debaa26dd49e5a7eb6c3b0ea7fd6433
skoriop <karthikp.ivy@gmail.com>
2024-06-07 15:08:11 +0530

    feat: add nextauth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+	providers: [
+		EmailProvider({
+			server: {
+				host: process.env.SMTP_HOST,
+				port: process.env.SMTP_PORT,
+				auth: {
+					user: process.env.SMTP_USER,
+					pass: process.env.SMTP_PASSWORD,
+				},
+			},
+			from: process.env.SMTP_FROM,
+			maxAge: 5 * 60,
+		}),
+	],
+	adapter: PrismaAdapter(prisma),
+});
+
+export { handler as GET, handler as POST };
