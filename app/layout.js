9ca891b58040342ccbe0395fd7ea03d135aaf710
skoriop <karthikp.ivy@gmail.com>
2024-06-07 16:20:48 +0530

    feat: improve styling

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={`${inter.className} w-screen h-screen`}>
 				<NextAuthProvider>{children}</NextAuthProvider>
 			</body>
 		</html>
--- app/page.js
+++ app/page.js
@@ -3,27 +3,43 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const IndexPage = () => {
-  const {data: session, status} = useSession();
+	const { data: session, status } = useSession();
 
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
+	if (status === "loading") {
+		return (
+			<div className="h-full flex flex-row items-center justify-center text-5xl">
+				Loading...
+			</div>
+		);
+	}
 
-  if (session) {
-    return (
-      <div>
-        Hello, {session.user?.email ?? session.user?.name} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        You are not logged in! <br />
-        <button onClick={() => signIn()}>Sign in</button>
-      </div>
-    );
-  }
+	if (session) {
+		return (
+			<div className="h-full flex flex-col items-center justify-center gap-12">
+				<div className="text-4xl">
+					Hello, {session.user?.email ?? session.user?.name}!
+				</div>
+				<button
+					className="text-2xl border rounded-lg py-2 px-4"
+					onClick={() => signOut()}
+				>
+					Sign out
+				</button>
+			</div>
+		);
+	} else {
+		return (
+			<div className="h-full flex flex-col items-center justify-center gap-12">
+				<div className="text-4xl">You are not logged in!</div>
+				<button
+					className="text-2xl border rounded-lg py-2 px-4"
+					onClick={() => signIn()}
+				>
+					Sign in
+				</button>
+			</div>
+		);
+	}
 };
 
 export default IndexPage;
