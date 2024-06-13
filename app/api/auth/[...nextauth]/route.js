887a2b0897a06e69ad84b15348315d5f38d3c60c
skoriop <karthikp.ivy@gmail.com>
2024-06-14 02:08:45 +0530

    feat: add metadata verification to verification token

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,26 +1,28 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import { CustomAdapter } from "@/app/lib/adapter";
 
 const prisma = new PrismaClient();
 
-const handler = NextAuth({
-	providers: [
-		EmailProvider({
-			server: {
-				host: process.env.SMTP_HOST,
-				port: process.env.SMTP_PORT,
-				auth: {
-					user: process.env.SMTP_USER,
-					pass: process.env.SMTP_PASSWORD,
+async function auth(req, res) {
+	return await NextAuth(req, res, {
+		providers: [
+			EmailProvider({
+				server: {
+					host: process.env.SMTP_HOST,
+					port: process.env.SMTP_PORT,
+					auth: {
+						user: process.env.SMTP_USER,
+						pass: process.env.SMTP_PASSWORD,
+					},
 				},
-			},
-			from: process.env.SMTP_FROM,
-			maxAge: 5 * 60,
-		}),
-	],
-	adapter: PrismaAdapter(prisma),
-});
+				from: process.env.SMTP_FROM,
+				maxAge: 5 * 60,
+			}),
+		],
+		adapter: CustomAdapter(prisma, req),
+	});
+}
 
-export { handler as GET, handler as POST };
+export { auth as GET, auth as POST };
